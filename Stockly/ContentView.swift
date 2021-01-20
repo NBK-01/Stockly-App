a18dea9384b412decc2ff8cbb189ec5e96ad7ac4
Nayef.k <nayefkanaan03@gmail.com>
2021-01-20 20:57:23 +0300

    Initial Commit

---
+++ Stockly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Stockly
+//
+//  Created by Nayef Kanaan on 20/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Stockly/StocklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocklyApp.swift
+//  Stockly
+//
+//  Created by Nayef Kanaan on 20/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct StocklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
